7eae1d9c36dfa2e8840217b0cc2b563116cd56d4
RayaFarshad <38769602+RayaFarshad@users.noreply.github.com>
2019-06-08 18:35:21 -0700

    Add files via upload

---
+++ newPHP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p>Click the button to demonstrate the prompt box.</p>
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+
+<?php
+//$num = 3; 
+function print10Times($num){  
+for ($x = 0; $x < $num; $x++) {   
+echo "Hello world - "; 
+}
+return "Hello World";
+}
+
+?>   
+
+
+</body>
+</html>
